4c2d7719c96ccc97d850ace5b1c80d7570e2c72c
dyllanwli <messsking@gmail.com>
2020-11-15 13:01:03 -0600

    update slide style

--- example/slide-bar/main.py
+++ example/slide-bar/main.py
@@ -1,42 +1,86 @@
 import streamlit as st
 import pandas as pd
 
-option = st.sidebar.selectbox(
-    'Options',
-     ('Active Layers and weighting', 'Social Sensing', 'Community Survey')
-)
+# option = st.sidebar.selectbox(
+#     'Options',
+#      ('Active Layers', 'Social Sensing', 'Community Survey')
+# )
 
-'#', option
+# '#', option
+
+st.write("""
+---
+
+## Multi-Scale Climate and Flood Simulation
+""")
+
+st.checkbox("Sea Level Rise")
+st.checkbox("Storm Surge")
+st.checkbox("Citizen-Science Based Flood Depth Mapping")
+st.checkbox("High-resolution Global Earth System Model")
+st.checkbox("Coupled Hygrologic-Hydraulic Flood Simulation Model")
+
+
+st.write("""
+---
+
+## Social Exposure
+""")
+if st.checkbox("Population Density"):
+    st.slider("Select Population Density")
+if st.checkbox("Poverty"):
+    st.slider("Select Poverty")
+if st.checkbox("Elderly"):
+    st.slider("Select Elderly")
+if st.checkbox("Employment"):
+    st.slider("Select Employment")
+if st.checkbox("Mobility and Human Dynamics"):
+    st.slider("Select Mobility and Human Dynamics")
+
+st.write("""
+---
+
+## Infrastructure Exposure
+""")
+if st.checkbox("Critical Facilities"):
+    st.slider("Select Critical Facilities")
+if st.checkbox("Development Patterns"):
+    st.slider("Select Development Patterns")
+
+st.write("""
+---
+
+## Ecosystem Exposure
+""")
+if st.checkbox("Natural Areas and Open Space"):
+    st.slider("Select Natural Areas and Open Space")
+if st.checkbox("Potential Pollution Sources"):
+    st.slider("Select Potential Pollution Sources")
+if st.checkbox("Natural Protection"):
+    st.slider("Select Natural Protection")
+if st.checkbox("Oil Spill"):
+    st.slider("Select Oil Spill")
+if st.checkbox("Wetland Potential"):
+    st.slider("Select Wetland Potential")
+
+st.write("""
+---
+""")
+
+if st.checkbox("Social Media"):
+    st.radio(
+        "Select Social Media User Type",
+        ('Single User', 'Group User')
+    )
+
 
 st.write("""
 ---
 """)
 
 
-if st.checkbox("Condition 1"):
-    x1 = st.slider("Select condition 1")
-    st.write("You selected", x1)
-
-if st.checkbox("Condition 2"):
-    x2 = st.slider("Select Condition 2")
-    st.write("You selected", x2)
-
-st.write("""
----
-""")
-
-if st.button("Run"):
-    st.write("RESULT")
-else:
-    st.write("Result shows here")
+st.button("Check Relevant Decision Problems")
+st.button("Revise Your Selections and Weights")
+st.button("Submit")
 
 
-st.write("""
----
-""")
-
-r = st.radio(
-    "Select ratio condition",
-    ('1', '2')
-)
-st.write("You selected", r)
