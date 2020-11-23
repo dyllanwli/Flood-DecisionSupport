d505003277d51e65fb335ec5a146a0c05a04fe5e
DY-L <dyllanwli@users.noreply.github.com>
2020-11-22 23:00:31 -0600

    Update main.py

--- example/slide-bar/main.py
+++ example/slide-bar/main.py
@@ -96,6 +96,8 @@ st.radio(
 st.button("Check Relevant Decision Problems")
 st.button("Submit")
 st.button("Sensitivity Report")
+st.button("Share Data and Results")
+
 
 st.markdown('<style>.st-c2{font-weight: 500;font-size:1.25rem;} .Widget>label{font-weight: 500;font-size:1.25rem;}</style>', unsafe_allow_html=True)
 
