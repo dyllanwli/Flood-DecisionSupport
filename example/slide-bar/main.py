d346d070d5cbc9c7ba3d28ddc239256923a27977
DY-L <dyllanwli@users.noreply.github.com>
2020-11-23 22:04:53 -0600

    Update main.py

--- example/slide-bar/main.py
+++ example/slide-bar/main.py
@@ -16,7 +16,7 @@ st.write("""
 st.checkbox("Sea Level Rise")
 st.checkbox("Storm Surge")
 st.checkbox("Citizen-Science Based Flood Depth Mapping")
-st.checkbox("High-resolution Global Earth System Model")
+st.checkbox("Regional Earth System Model - Drought Predictions")
 st.checkbox("Coupled Hygrologic-Hydraulic Flood Simulation Model")
 st.multiselect("USGS Data",
     options=("USGS Data 1", "USGS Data 2")
