dea0e5d1cb93db68437e77d76aee3f104c759c98
dyllanwli <messsking@gmail.com>
2021-01-23 04:49:03 +0000

    get vue-mapbox done

---
+++ example/mb-app/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-app>
+    <v-app-bar
+      max-height=40
+      app
+      color="primary"
+      dark
+      class="main-app-bar"
+    >
+    </v-app-bar>
+
+    <v-main>
+      <BaseMap/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import BaseMap from '@/components/BaseMap';
+// import Router from './components/Router'
+export default {
+  name: 'App',
+
+  components: {
+    BaseMap,
+    // Router
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ example/mb-app/src/components/Router/index.vue
@@ -0,0 +1,4 @@
+<template src="./Router.html"></template>
+<script src="./Router.js"></script>
+<style src="./Router.scss" scoped lang="scss"></style>
+
